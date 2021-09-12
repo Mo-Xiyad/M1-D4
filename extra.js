0ceba82e03a5a306812851213b2ca6b4b6530247
Zee <xziyad92@gmail.com>
2021-09-12 16:01:12 +0200

    extra questions

---
+++ extra.js
@@ -0,0 +1,131 @@
+let isMale = false
+// let isMale = true ? 'male' : 'female'
+
+/* let result = isMale === true ? 'Male' : 'Female'
+
+console.log(result)
+ */
+
+/*
+2. Write code to check from two given integers whether one of them is 8 or their sum or difference is 8.
+*/
+
+/* let num1 = 2
+let num2 = 7
+
+if (num1 === 8 || num2 === 8 || num1 + num2 === 8 || num1 - num2 === 8) {
+    console.log(true)
+} else {
+    console.log(false)
+} */
+
+/*
+  3. Create a variable which concatenates two strings.
+*/
+
+/* let str = 'One' + ' two'
+
+console.log(str) */
+
+
+/*
+4.Write a JavaScript conditional statement to sort three numbers. Display the result in the console.
+*/
+
+/* let x = 1
+let y = 5
+let z = 2
+
+if (x < y && x < z) {
+    if (y < z) {
+        console.log(x + ', ' + y + ', ' + z)
+        console.log(`con 1`)
+    } else {
+        console.log(x + ', ' + z + ', ' + y)
+        console.log(`con 1`)
+    }
+} else if (y < x && y < z) {
+    if (x < z) {
+        console.log(y + ', ' + x + ', ' + z)
+        console.log(`con 2`)
+    } else {
+        console.log(y + ', ' + z + ', ' + x)
+        console.log(`con 2`)
+    }
+} else if (z < x && z < y) {
+    if (x < y) {
+        console.log(z + ', ' + x + ', ' + y)
+        console.log(`con 3`)
+    } else
+        console.log(z + ', ' + y + ', ' + x)
+    console.log(`con 3`)
+}
+ */
+
+
+/*
+ 5. Write code to find the average of two given integers.
+*/
+
+/* let x = 25
+let y = 78
+
+let result = (x + y) / 2
+
+console.log(result)
+*/
+
+/*
+  6. Find the longest of two given strings
+*/
+
+/* let a = 'hellw world'
+let b = `This is a new string`
+
+if (a.length > b.length) {
+    console.log('a is greater')
+} else {
+    console.log('b is greater')
+} */
+
+
+/*
+ 7. Write code to check whether a value is an integer or not.
+*/
+
+/* let x = 5
+
+console.log(typeof x)
+
+if (typeof x === 'number') {
+    console.log(true)
+} else {
+    console.log(false)
+}
+ */
+
+
+/*
+ 8. Write code to calculate the percentage (%) of a number.
+        Ex.: 20% of 400 is 80
+*/
+
+/* let price = 400
+let percent = 20
+let result = (price * percent) / 100
+console.log(result)
+ */
+
+/*
+ 9. Write code to check if a given number is even or odd.
+*/
+
+/*
+let n = 5
+
+if (n % 2 === 0) {
+    console.log('even')
+} else {
+    console.log('odd')
+}
+ */
